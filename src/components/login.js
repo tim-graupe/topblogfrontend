09fea88e5d0e628210556e7d8e9da8c07bb64647
tim-graupe <tgraupe90@gmail.com>
2023-06-13 19:34:34 -0400

    form changes

--- src/components/login.js
+++ src/components/login.js
@@ -4,61 +4,50 @@ import { Link } from "react-router-dom";
 export const Login = () => {
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  // const [error, setError] = useState("");
 
-  const handleLogin = async (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
-    setError("");
-
-    try {
-      const response = await fetch(
-        "https://topblogbackend-production.up.railway.app/log-in",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ username, password }),
-        }
-      );
-      if (response.ok) {
-        // Login successful
-        console.log("response");
-      } else {
-        // Login failed
-        console.log(response);
-      }
-    } catch (error) {
-      console.error("Login failed:", error);
-    }
+    fetch("https://topblogbackend-production.up.railway.app/log-in", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: username,
+        password: password,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => console.log("response"));
   };
 
   return (
-    <div>
-      <form onSubmit={handleLogin}>
-        <h2>Login</h2>
-        {error && <div>{error}</div>}
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUserName(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      <Link to={"/sign_up"}>New user</Link>
-    </div>
+    <form onSubmit={handleLogin}>
+      <h2>Login</h2>
+      {/* {error && <div>{error}</div>} */}
+      <div>
+        <label>Username:</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUserName(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </div>
+      <button type="submit">Login</button>
+      <br></br>
+      <Link to={"/sign_up"}>New user?</Link>
+    </form>
   );
 };
